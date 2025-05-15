7dd6618a55d1d92060223a19272702b33da945dd
vihdutta <vihdutta@gmail.com>
2025-05-15 18:27:16 -0400

    implemented endpoint returning excel schedule, overview, and LO data

---
+++ files/populate_weeks/populate_weeks.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import yaml
+import numpy as np
+
+
+# populates the Week objects from the yaml and excel schedule files
+def populate_weeks(
+    excel_schedule_path: str,
+    overview_path: str,
+    objectives_path: str,
+):
+    DATA_START_ROW = 1
+    df = pd.read_excel(excel_schedule_path, engine="openpyxl")
+    weeks_column = df.iloc[DATA_START_ROW:, 1].ffill().astype(int)
+    df = df.replace(np.nan, "")
+
+    overview_data, objective_data = read_overview_and_objective_yaml(overview_path, objectives_path)
+
+    weeks = {}
+    for w in set(weeks_column):
+        weeks[w] = {}
+        weeks[w]["module"] = ""
+        weeks[w]["dates"] = []
+        weeks[w]["referenced"] = []
+        weeks[w]["assigned"] = []
+        weeks[w]["due"] = []
+        weeks[w]["overview_statement"] = overview_data[w]["description"]
+
+    for index, row in df.iterrows():
+        if index == 0:
+            continue
+        if "Mod" not in str(weeks[weeks_column[index]]["module"]) and str(row[2]) != "":
+            weeks[weeks_column[index]]["module"] = int("".join(c for c in row[2] if c.isdigit()))
+        elif str(row[2]) == "":
+            weeks[weeks_column[index]]["module"] = weeks[weeks_column[index - 1]]["module"]
+
+        weeks[weeks_column[index]]["dates"].append(row[4])
+        weeks[weeks_column[index]]["referenced"].append(row[7])
+        weeks[weeks_column[index]]["assigned"].append(row[9])
+        weeks[weeks_column[index]]["due"].append(row[10])
+
+    for w in set(weeks_column):
+        weeks[w]["learning_objectives"] = objective_data[weeks[w]["module"]]["learning_objectives"]
+
+    return weeks
+
+def read_overview_and_objective_yaml(
+    overview_path: str,
+    objectives_path: str
+):
+    overview_data = {}
+    objective_data = {}
+    if overview_path:
+        with open(overview_path, "r", encoding="utf-8") as f:
+            overview_data = yaml.safe_load(f)
+    if objectives_path:
+        with open(objectives_path, "r", encoding="utf-8") as f:
+            objective_data = yaml.safe_load(f)
+
+    return (overview_data, objective_data)
+
+
+if __name__ == "__main__":
+    excel_schedule_path = "files/yaml/schedule.xlsx"
+    overview_path = "files/yaml/overview_statements.yaml"
+    objectives_path = "files/yaml/learning_objectives.yaml"
+
+    weeks = populate_weeks(
+        excel_schedule_path=excel_schedule_path,
+        overview_path=overview_path,
+        objectives_path=objectives_path
+    )
+    print(weeks)
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-from datetime import datetime, timedelta
-from files.classes.week import Week
+from datetime import timedelta
+from files.populate_weeks.populate_weeks import populate_weeks
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -11,18 +10,8 @@ templates = Jinja2Templates(directory="templates")
 templates.env.filters["add_days"] = lambda value, days: value + timedelta(days=days)
 templates.env.filters["strftime"] = lambda value, fmt: value.strftime(fmt)
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 async def root(request: Request):
-    today = datetime.today()
-    context = {
-        "request": request,
-        "week_number": 5,
-        "week_start": today,
-        "last_week_text": "last_week_text",
-        "next_week_text": "next_week_text",
-        "full_course_schedule_text": "See the Full Course Schedule",
-        "module_number": 3,
-        "module_number_learning_objectives": "Understand FastAPI & Jinja integration"
-    }
-    
-    return {templates.TemplateResponse("index.html", context)}
+    weeks = populate_weeks("files/yaml/schedule.xlsx", "files/yaml/overview_statements.yaml", "files/yaml/learning_objectives.yaml")
+    return {"message": weeks}
+    # return {templates.TemplateResponse("index.html", context)}
